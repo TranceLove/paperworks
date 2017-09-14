75efa9f75608be029f76b9c856f0194adc2ff164
TranceLove <airwave209gt@gmail.com>
2017-09-14 17:30:45 +0800

    Initial client app layout

---
+++ client/app/app.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+export default class PaperWorkApp extends React.Component
+{
+    render()
+    {
+        return (
+            <View>
+                <Text>Hello world</Text>
+            </View>
+        )
+    }
+}
