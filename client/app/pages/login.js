0e0b9666674ec6953ddd28a8346e70fdd0df2ac5
TranceLove <airwave209gt@gmail.com>
2017-09-22 13:14:54 +0800

    Better login screen

--- client/app/pages/login.js
+++ client/app/pages/login.js
@@ -2,9 +2,32 @@
 
 import React from "react";
 import { Text, View, Card } from 'react-native';
+import { Button } from 'react-native-elements'
+import { Styles } from './../styles/styles'
 
 export default ({navigation}) => (
-    <View>
-
+    <View style={Styles.Login.content}>
+        <View style={Styles.Login.container}>
+            <Button
+                title="Login with Twitter"
+                icon={{name: 'twitter', type: 'entypo'}}
+                style={Styles.Login.twitterLogin}
+                backgroundColor="#1dcaff"
+                fontSize={20}
+            />
+            <Button
+                title="Login with Facebook"
+                icon={{name: 'facebook', type: 'entypo'}}
+                style={Styles.Login.facebookLogin}
+                backgroundColor="#3b5998"
+                fontSize={20}
+            />
+            <Button
+                title="Continue without login"
+                icon={{name: 'map', type: 'entypo'}}
+                fontSize={19}
+                onPress={() => console.log("Pressed")}
+            />
+        </View>
     </View>
 )
---
+++ client/app/styles/styles.js
@@ -0,0 +1,28 @@
+"use strict"
+
+import React from 'react';
+import { StyleSheet } from 'react-native';
+
+let loginStyle = StyleSheet.create({
+    content: {
+        flex:1,
+        alignItems:'stretch',
+        justifyContent:'center'
+    },
+    container: {
+        height: 180,
+        justifyContent:"space-between"
+    },
+    twitterLogin: {
+        marginBottom: 10,
+        padding: 10
+    },
+    facebookLogin: {
+        padding: 10,
+        marginTop: 10
+    }
+})
+
+export const Styles = {
+    Login: loginStyle
+}
