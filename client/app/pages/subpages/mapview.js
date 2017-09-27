1bc01bbb7f0fee3cbab7762a59fb351b65c7c0e4
TranceLove <airwave209gt@gmail.com>
2017-09-27 12:15:06 +0800

    Disable console.log()

--- client/app/pages/subpages/mapview.js
+++ client/app/pages/subpages/mapview.js
@@ -5,12 +5,18 @@ import MapView from 'react-native-maps';
 import { Styles } from './../../styles/styles'
 import ProfileService from './../../services/ProfileService'
 
-export default ({navigation}) => (
+let mapView = ({navigation}) => (
     <MapView style={Styles.MainView.map}
         region={ProfileService.myLastLocation()}
         showsCompass={true}
         showsScale={true}
         loadingEnabled={true}
-        onRegionChange={(region)=> console.log(region.latitudeDelta + ", " + region.longitudeDelta)}
+        //onRegionChange={(region)=> console.log(region.latitudeDelta + ", " + region.longitudeDelta)}
     />
 )
+
+mapView.navigationOptions = ({ navigation }) => {
+    drawerLabel: 'Home'
+}
+
+export default mapView
