f3f6e771f44e109603851e8fcd5da316114f445e
TranceLove <airwave209gt@gmail.com>
2017-09-27 12:17:29 +0800

    Initial implementation of DrawerNavigator integration

--- client/app/pages/mainview.js
+++ client/app/pages/mainview.js
@@ -1,6 +1,9 @@
 "use strict"
 
+import React from 'react';
+import { TouchableOpacity } from 'react-native'
 import { DrawerNavigator } from "react-navigation"
+import { Icon } from 'react-native-elements'
 import MapView from './subpages/mapview'
 import ProfileView from './subpages/profile'
 
@@ -13,6 +16,26 @@ let mainView = DrawerNavigator({
         path: "/profile",
         screen: ProfileView
     }
+},{
+    drawerPosition: "left",
+    drawerWidth: 200
+})
+
+mainView.navigationOptions = ({navigation}) => ({
+    headerTitle: "Paperworks",
+    headerLeft: (
+        <TouchableOpacity style={{paddingHorizontal:20}} onPress={() => {
+            if (navigation.state.index === 0) {
+                // check if drawer is not open, then only open it
+                navigation.navigate('DrawerOpen');
+            } else {
+                // else close the drawer
+                navigation.navigate('DrawerClose');
+            }
+        }}>
+            <Icon type="entypo" name="menu" size={24} />
+        </TouchableOpacity>
+    )
 })
 
 export default mainView
