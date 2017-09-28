79353076f69642e97345f05b3281149ae3402d09
TranceLove <airwave209gt@gmail.com>
2017-09-28 15:37:11 +0800

    import github.com/appleboy/gin-jwt

--- server/src/github.com/TranceLove/paperworks/main.go
+++ server/src/github.com/TranceLove/paperworks/main.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/appleboy/gin-jwt"
 )
 
 func main(){
-    r := gin.Default()
+	r := gin.New()
+	r.Use(gin.Logger())
+	r.Use(gin.Recovery())
 
     r.Run(":8080")
 }
