59d66eb9621aaa80cdc2765eb63716ec56a3459d
TranceLove <airwave209gt@gmail.com>
2017-10-17 12:47:07 +0800

    /hello, / test

---
+++ server/src/github.com/TranceLove/paperworks/spec/routes/hello_test.go
@@ -0,0 +1,51 @@
+package routes
+
+import (
+    "os"
+    "fmt"
+    "net/http"
+    "testing"
+
+    "github.com/TranceLove/paperworks/routes"
+
+    "github.com/buger/jsonparser"
+
+    "github.com/appleboy/gofight"
+    "github.com/stretchr/testify/suite"
+)
+
+type HelloTestSuite struct {
+    suite.Suite
+}
+
+func (suite *HelloTestSuite) SetupSuite(){
+    cwd, _ := os.Getwd()
+    os.Setenv("PAPERWORKS_CONFIG", fmt.Sprintf("%s/../test_config.json", cwd))
+    os.Setenv("PAPERWORKS_ENV", "test")
+}
+
+func (suite *HelloTestSuite) TestRoot(){
+    r := gofight.New()
+
+    r.GET("/").
+		SetDebug(true).
+		Run(routes.Init(), func(r gofight.HTTPResponse, rq gofight.HTTPRequest) {
+			suite.Equal(http.StatusNotFound, r.Code)
+    })
+}
+
+func (suite *HelloTestSuite) TestHello(){
+    r := gofight.New()
+
+    r.GET("/hello").
+		SetDebug(true).
+		Run(routes.Init(), func(r gofight.HTTPResponse, rq gofight.HTTPRequest) {
+			suite.Equal(http.StatusOK, r.Code)
+            verify, _ := jsonparser.GetString([]byte(r.Body.String()), "message")
+            suite.Equal("Hello World", verify)
+    })
+}
+
+func TestExampleTestSuite(t *testing.T) {
+    suite.Run(t, new(HelloTestSuite))
+}
