914d990aec01844042c9cb7446af196461407f8e
TranceLove <airwave209gt@gmail.com>
2017-10-17 12:46:44 +0800

    Move gin router creation to individual function, to facilitate route testing

--- server/src/github.com/TranceLove/paperworks/main.go
+++ server/src/github.com/TranceLove/paperworks/main.go
@@ -3,18 +3,13 @@ package main
 import (
 	"net/http"
 
-	"github.com/gin-gonic/gin"
 	"github.com/TranceLove/paperworks/routes"
 	// "github.com/appleboy/gin-jwt"
 )
 
 func main(){
 
-	r := gin.New()
-	r.Use(gin.Logger())
-	r.Use(gin.Recovery())
-
-	routes.Init(r)
+	r := routes.Init()
 
     http.ListenAndServe(":3000", r)
 }
--- server/src/github.com/TranceLove/paperworks/routes/init.go
+++ server/src/github.com/TranceLove/paperworks/routes/init.go
@@ -4,7 +4,13 @@ import (
     "github.com/gin-gonic/gin"
 )
 
-func Init(r *gin.Engine) {
+func Init() *gin.Engine {
+    r := gin.New()
+    r.Use(gin.Logger())
+    r.Use(gin.Recovery())
+
     r.GET("/hello", hello)
 	r.GET("/random-image", randomImage)
+
+    return r
 }
