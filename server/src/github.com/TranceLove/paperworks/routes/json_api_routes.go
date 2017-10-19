a8221a04462c65d41bda3b400281acba439b4769
TranceLove <airwave209gt@gmail.com>
2017-10-19 11:29:50 +0800

    Initial implementation of verifyRequestValidity()

---
+++ server/src/github.com/TranceLove/paperworks/routes/json_api_routes.go
@@ -0,0 +1,13 @@
+package routes
+
+import (
+    "net/http"
+    "github.com/gin-gonic/gin"
+)
+
+func verifyRequestValidity(c *gin.Context){
+    if(c.GetHeader(CONTENT_TYPE_HEADER) != CONTENT_TYPE_JSON_API){
+        c.AbortWithStatus(http.StatusUnsupportedMediaType)
+        return
+    }
+}
