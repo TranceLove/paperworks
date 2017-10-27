24c1693d737e777b3b341d5f71c24d88e2c8ae7c
TranceLove <airwave209gt@gmail.com>
2017-10-27 15:47:46 +0800

    Convert declaration method of login and select server views to reduce unnecessary passing of variables

--- client/app/pages/login.js
+++ client/app/pages/login.js
@@ -6,39 +6,47 @@ import { Button } from 'react-native-elements'
 import { Styles } from './../styles/styles'
 import { NavigationActions } from 'react-navigation'
 
-function login(navigation, method)
+class LoginView extends React.Component
 {
-    navigation.dispatch(NavigationActions.reset({
-        index: 0,
-        actions: [
-            NavigationActions.navigate({routeName:"MainView"})
-        ]
-    }))
+    doLogin(method:String)
+    {
+        this.props.navigation.dispatch(NavigationActions.reset({
+            index: 0,
+            actions: [
+                NavigationActions.navigate({routeName:"MainView"})
+            ]
+        }))
+    }
+
+    render()
+    {
+        return (
+            <View style={Styles.Login.content}>
+                <View style={Styles.Login.container}>
+                    <Button
+                        title="Login with Twitter"
+                        icon={{name: 'twitter', type: 'entypo'}}
+                        style={Styles.Login.twitterLogin}
+                        backgroundColor="#1dcaff"
+                        fontSize={20}
+                    />
+                    <Button
+                        title="Login with Facebook"
+                        icon={{name: 'facebook', type: 'entypo'}}
+                        style={Styles.Login.facebookLogin}
+                        backgroundColor="#3b5998"
+                        fontSize={20}
+                    />
+                    <Button
+                        title="Continue without login"
+                        icon={{name: 'map', type: 'entypo'}}
+                        fontSize={19}
+                        onPress={() => this.doLogin("nologin")}
+                    />
+                </View>
+            </View>
+        )
+    }
 }
 
-export default ({navigation}) => (
-    <View style={Styles.Login.content}>
-        <View style={Styles.Login.container}>
-            <Button
-                title="Login with Twitter"
-                icon={{name: 'twitter', type: 'entypo'}}
-                style={Styles.Login.twitterLogin}
-                backgroundColor="#1dcaff"
-                fontSize={20}
-            />
-            <Button
-                title="Login with Facebook"
-                icon={{name: 'facebook', type: 'entypo'}}
-                style={Styles.Login.facebookLogin}
-                backgroundColor="#3b5998"
-                fontSize={20}
-            />
-            <Button
-                title="Continue without login"
-                icon={{name: 'map', type: 'entypo'}}
-                fontSize={19}
-                onPress={() => login(navigation, "nologin")}
-            />
-        </View>
-    </View>
-)
+export default LoginView
--- client/app/pages/selectserver.js
+++ client/app/pages/selectserver.js
@@ -7,28 +7,34 @@ import { Styles } from './../styles/styles'
 import Environment from './../services/Environment'
 import { NavigationActions } from 'react-navigation'
 
-function setServer(navigation,serverName)
+class SelectServerView extends React.Component
 {
-    Environment.set("serverName", serverName)
-    navigation.dispatch(NavigationActions.reset({
-        index: 0,
-        actions: [
-            NavigationActions.navigate({routeName:"Login"})
-        ]
-    }))
+    static navigationOptions = {}
+
+    setServer(serverName)
+    {
+        Environment.set("serverName", serverName)
+        this.props.navigation.dispatch(NavigationActions.reset({
+            index: 0,
+            actions: [
+                NavigationActions.navigate({routeName:"Login"})
+            ]
+        }))
+    }
+
+    render()
+    {
+        return (
+            <View style={Styles.SelectServer.content}>
+                <View style={Styles.SelectServer.container}>
+                    <Button title="localhost" style={Styles.SelectServer.btnLocalhost} onPress={()=>{this.setServer('localhost')}} fontSize={20} />
+                    <Button title="Development" style={Styles.SelectServer.btnDevServer} onPress={()=>{this.setServer('development')}} fontSize={20} />
+                    <Button title="Staging" style={Styles.SelectServer.btnStagingServer} onPress={()=>{this.setServer('staging')}} fontSize={20} />
+                    <Button title="Production" style={Styles.SelectServer.btnProductionServer} onPress={()=>{this.setServer('production')}} fontSize={20} />
+                </View>
+            </View>
+        )
+    }
 }
 
-let SelectServer = ({navigation}) => (
-    <View style={Styles.SelectServer.content}>
-        <View style={Styles.SelectServer.container}>
-            <Button title="localhost" style={Styles.SelectServer.btnLocalhost} onPress={()=>{setServer(navigation,'localhost')}} fontSize={20} />
-            <Button title="Development" style={Styles.SelectServer.btnDevServer} onPress={()=>{setServer(navigation,'development')}} fontSize={20} />
-            <Button title="Staging" style={Styles.SelectServer.btnStagingServer} onPress={()=>{setServer(navigation,'staging')}} fontSize={20} />
-            <Button title="Production" style={Styles.SelectServer.btnProductionServer} onPress={()=>{setServer(navigation,'production')}} fontSize={20} />
-        </View>
-    </View>
-)
-
-SelectServer.navigationOptions = {}
-
-export default SelectServer
+export default SelectServerView
